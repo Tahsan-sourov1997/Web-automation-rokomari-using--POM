e859de93be23ccdfa97ffdde4d7b508f15678d10
Tahsan-sourov1997 <tahsanhossain776@gmail.com>
2025-04-03 00:37:07 +0600

    updated code

--- src/test/java/pages/WritterPage.java
+++ src/test/java/pages/WritterPage.java
@@ -3,14 +3,16 @@ package pages;
 import org.openqa.selenium.By;
 
 public class WritterPage extends BasePage{
-    public By writter_element =By.xpath("//a[@id='js--authors']");
+    public By writter_element =By.xpath("//body/div/div[2]/div[1]/div[1]/a[1]");
     public By writter_humayan = By.xpath("//a[contains(text(),'হুমায়ূন আহমেদ')]");
     public By somokalin_text = By.xpath("//label[contains(text(),'সমকালীন উপন্যাস')]");
     public By rochona_text = By.xpath("//label[@for='rok-filter-categoryIds-1321']");
     public By add_cross_btn = By.xpath("//span[@aria-hidden='true']");
     public By page_arrow_btn = By.xpath("//div[@class='browse-page']//a[4]");
     public By second_page_btn = By.xpath("//a[normalize-space()='2']");
-    public By cart_kothau_keu_nai = By.xpath("//h4[contains(text(),'কোথাও কেউ নেই')]");
-    public By add_cart_btn = By.xpath("//div[@title='কোথাও কেউ নেই হুমায়ূন আহমেদ']//div[1]//div[3]//button[1]");
+    public By books_of_humayan = By.xpath("//h1[contains(text(),'হুমায়ূন আহমেদ এর বই সমূহ')]");
+    public By cart_basor = By.xpath("//div[@title='বাসর হুমায়ূন আহমেদ']//div[2]//p[1]");
+    public By add_cart_btn = By.xpath("//div[@title='বাসর হুমায়ূন আহমেদ']//div[1]//div[3]//button[1]");
     public By cart_quantity = By.xpath("//span[@class='js--cart-quantity']");
+    public By checkout_btn = By.xpath("//span[normalize-space()='Proceed to Checkout']");
 }
--- src/test/java/testcases/TestWritterPage.java
+++ src/test/java/testcases/TestWritterPage.java
@@ -13,7 +13,8 @@ public class TestWritterPage extends driverSetup{
     @Test
     public void testWritterPage() throws InterruptedException {
         homePage.loadAnWebPage(homePage.home_url);
-        homePage.clickElement(homePage.cross_btn);
+        Thread.sleep(20000);
+//        homePage.clickElement(homePage.cross_btn);
         Actions actions = new Actions(getDriver());
         actions.moveToElement(writterPage.getElement(writterPage.writter_element)).build().perform();
         Thread.sleep(5000);
@@ -21,21 +22,25 @@ public class TestWritterPage extends driverSetup{
         Thread.sleep(3000);
         actions.scrollToElement(writterPage.getElement(writterPage.rochona_text)).build().perform();
         Thread.sleep(5000);
-        writterPage.clickElement(writterPage.add_cross_btn);
+//        writterPage.clickElement(writterPage.add_cross_btn);
         writterPage.clickElement(writterPage.somokalin_text);
         actions.scrollToElement(writterPage.getElement(writterPage.rochona_text)).build().perform();
         writterPage.clickElement(writterPage.rochona_text);
-        actions.scrollToElement(writterPage.getElement(writterPage.page_arrow_btn)).build().perform();
+        actions.moveToElement(writterPage.getElement(writterPage.page_arrow_btn)).build().perform();
         Thread.sleep(5000);
         writterPage.clickElement(writterPage.second_page_btn);
-        actions.scrollToElement(writterPage.getElement(writterPage.cart_kothau_keu_nai)).build().perform();
+        actions.scrollToElement(writterPage.getElement(writterPage.rochona_text)).build().perform();
+        actions.moveToElement(writterPage.getElement(writterPage.cart_basor));
         Thread.sleep(3000);
-        actions.moveToElement(writterPage.getElement(writterPage.cart_kothau_keu_nai)).build().perform();
+        actions.moveToElement(writterPage.getElement(writterPage.cart_basor)).build().perform();
         writterPage.clickElement(writterPage.add_cart_btn);
         Thread.sleep(4000);
         String cart_number = writterPage.getElement(writterPage.cart_quantity).getText();
 //        System.out.println(cart_number);
         Assert.assertEquals(cart_number, "1");
+        writterPage.clickElement(writterPage.cart_quantity);
+        Thread.sleep(3000);
+        writterPage.clickElement(writterPage.checkout_btn);
 
 
 
